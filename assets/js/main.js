6cf688d660ca1149fa2614211222a5a2d6602c20
RiccardoGrande <grandericcardo1997@gmail.com>
2023-03-15 16:20:29 +0100

    try to make it appear in HTML

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+//creo un array con la lista della spesa
+
+const shopList = [
+
+    'banana',
+    'mela',
+    'pera'
+]
+
+//creo una variabile while
+
+let listElement = 0;
+let i = 0;
+
+while ( i < shopList.length ) {
+    
+    let listElement = shopList[i];
+
+    console.log(listElement);
+
+    i++
+    
+    document.createElement(listElement)
+    
+}
+
+   
+
+
